50b95d74dd073e22ba5e39bc950a309393c44c8c
adriendomoison <adomoison@gmail.com>
2018-06-12 12:01:36 -0700

    make HttpRequestHandler return an ErrorDescription instead of ApiErrors

--- http.go
+++ http.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"github.com/mitchellh/mapstructure"
+	"errors"
 )
 
 // RequestHeader is the object to send to the HttpRequestHandlers
@@ -16,8 +18,26 @@ type RequestHeader struct {
 	Authorization string
 }
 
+// BuildErrorDescriptionFromApiError rebuild an ErrorDescription object out of an ApiError object
+func BuildErrorDescriptionFromApiError(apiErrors ApiErrors, code int) (*ErrorDescription) {
+	if len(apiErrors.Errors) > 0 {
+		var err Error
+		mapstructure.Decode(apiErrors.Errors[0], &err)
+		return &ErrorDescription{
+			Detail:  errors.New(err.Detail),
+			Message: err.Message,
+			Param:   err.Param,
+			Code:    Code(code),
+		}
+	}
+	return &ErrorDescription{
+		Detail:  errors.New("unknown error"),
+		Code:    Code(code),
+	}
+}
+
 // HttpRequestHandler is a handler for easy http requests
-func HttpRequestHandler(requestHeader RequestHeader, requestBody interface{}, responseDTO interface{}) (*http.Response, ApiErrors) {
+func HttpRequestHandler(requestHeader RequestHeader, requestBody interface{}, responseDTO interface{}) (*http.Response, *ErrorDescription) {
 	req, err := http.NewRequest(requestHeader.Method, requestHeader.URL, encodeRequestBody(requestBody))
 	req.Header.Set("Content-Type", requestHeader.ContentType)
 	req.Header.Set("Authorization", requestHeader.Authorization)
@@ -34,7 +54,8 @@ func HttpRequestHandler(requestHeader RequestHeader, requestBody interface{}, re
 	var apiErrors ApiErrors
 	json.Unmarshal(body, &responseDTO)
 	json.Unmarshal(body, &apiErrors)
-	return resp, apiErrors
+
+	return resp, BuildErrorDescriptionFromApiError(apiErrors, resp.StatusCode)
 }
 
 func encodeRequestBody(reqBody interface{}) io.Reader {
